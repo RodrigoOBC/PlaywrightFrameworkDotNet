27443568785380ce421fe8619f3fc80dc2b4ee4b
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-04-09 19:23:16 -0300

    Update FormPageTest to include filePath in data row and method parameters for form submission

--- PlaywrightTests/Tests/formPageTest.cs
+++ PlaywrightTests/Tests/formPageTest.cs
@@ -10,13 +10,13 @@ namespace PlaywrightTests;
 public class FormPageTest : PageTest
 {
     [DataTestMethod]
-    [DataRow("joão","ninguem","joao@teste.com","2020555999","28 Dec 1996","Male","street new","Computer Science","Uttar Pradesh","Agra")]
+    [DataRow("joão","ninguem","joao@teste.com","2020555999","28 Dec 1996","Male","street new","Computer Science","Uttar Pradesh","Agra", "../../../temp/Bruce_Wayne_06.jpg")]
     [Description("Search for different products and verify results.")]
-    public async Task Submit_WithValidDatas_Sucessfull(string firstName, string lastName, string email, string mobile,string date,string gender, string currentAddress, string subject,string state, string city)
+    public async Task Submit_WithValidDatas_Sucessfull(string firstName, string lastName, string email, string mobile,string date,string gender, string currentAddress, string subject,string state, string city, string filePath)
     {
         var formPage = new FormPage(Page);
         await formPage.NavigateToAsync();
-        await formPage.CompleteForm(firstName,lastName,email,mobile,date,gender,currentAddress,subject,state,city);
+        await formPage.CompleteForm(firstName,lastName,email,mobile,date,gender,currentAddress,subject,state,city,filePath);
         await formPage.ClickSubmitButton();
     }
 }
