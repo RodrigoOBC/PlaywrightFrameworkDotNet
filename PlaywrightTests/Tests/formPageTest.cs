0568289d00b1170d5521c36b7974f78bd2d6b79a
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-04-09 19:27:49 -0300

    Add additional data row for Joana in FormPageTest to enhance test coverage

--- PlaywrightTests/Tests/formPageTest.cs
+++ PlaywrightTests/Tests/formPageTest.cs
@@ -11,6 +11,7 @@ public class FormPageTest : PageTest
 {
     [DataTestMethod]
     [DataRow("joão","ninguem","joao@teste.com","2020555999","28 Dec 1996","Male","street new","Computer Science","Uttar Pradesh","Agra", "../../../temp/Bruce_Wayne_06.jpg")]
+    [DataRow("Joana","ninguem","joana@teste.com","2020555999","29 Dec 1996","Female","street new","Maths","Uttar Pradesh","Agra", "../../../temp/Bruce_Wayne_06.jpg")]
     [Description("Search for different products and verify results.")]
     public async Task Submit_WithValidDatas_Sucessfull(string firstName, string lastName, string email, string mobile,string date,string gender, string currentAddress, string subject,string state, string city, string filePath)
     {
